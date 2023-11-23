5a65fd4b130cf67d2027aac86cf60d43eb75083f
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-23 10:47:52 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-
-cd   /home/ubuntu/Back_Flask
-
+# 프로젝트 디렉터리로 이동
+cd /home/ubuntu/Back_Flask
 
 echo ">>> pip install ----------------------"
 pip install -r requirements.txt
 
-
 echo ">>> remove template files ------------"
 rm -rf appspec.yml requirements.txt
-
 
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/Back_Flask
 
-
 echo ">>> run app --------------------------"
-flask run
+chmod +x app.sh
+./app.sh
