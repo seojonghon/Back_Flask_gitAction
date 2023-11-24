eac1466d176c3c3d472d4f5dc6c764b85eeb1e21
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-24 15:23:33 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,16 +4,9 @@
 cd   /home/ubuntu/Back_Flask
 
 
+
 echo ">>> pip install ----------------------"
 pip install -r requirements.txt
-
-
-echo ">>> remove template files ------------"
-rm -rf appspec.yml requirements.txt
-
-
-echo ">>> change owner to ubuntu -----------"
-chown -R ubuntu /home/ubuntu/Back_Flask
 
 echo ">>> set env --------------------------"
 export FLASK_APP=app
@@ -26,3 +19,12 @@ export DB_PORT=15432
 export DB_NAME=recipe
 export SECRET_KEY=b'X2t\x05\xf5\xcb\xc1\xa5`\xd6"\xfb \x0c\x14\xb5'
 export JWT_SECRET_KEY=b'\x10\xc8\xf9S\x1c&\x9fAD\x82\xf5\xa1\xdbx\xea>'
+
+echo ">>> remove template files ------------"
+rm -rf appspec.yml requirements.txt
+
+
+echo ">>> change owner to ubuntu -----------"
+chown -R ubuntu /home/ubuntu/Back_Flask
+
+
