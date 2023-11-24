b5481745280ec9c3ab10c9e362a4750cc09f223d
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-24 17:32:25 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,19 +8,6 @@ cd   /home/ubuntu/Back_Flask
 echo ">>> pip install ----------------------"
 pip install -r requirements.txt
 
-echo ">>> set env --------------------------"
-export FLASK_APP=app
-export REDIS_HOST=redis
-export APP_CONFIG_FILE=/home/ubuntu/Back_Flask/config/production.py
-export DB_USER=postgres
-export DB_PASSWORD=password
-export DB_HOST=hwan515.synology.me
-export DB_PORT=15432
-export DB_NAME=recipe
-export SECRET_KEY=b'X2t\x05\xf5\xcb\xc1\xa5`\xd6"\xfb \x0c\x14\xb5'
-export JWT_SECRET_KEY=b'\x10\xc8\xf9S\x1c&\x9fAD\x82\xf5\xa1\xdbx\xea>'
-
-export TEST=1234
 
 echo ">>> remove template files ------------"
 rm -rf appspec.yml requirements.txt
