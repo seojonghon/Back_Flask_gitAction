ac18299bdc05fa6b5aad329e801a9ccefeeb84e4
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-27 14:30:39 +0900

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -6,6 +6,6 @@ export DB_USER=postgres
 export DB_PASSWORD=password
 export DB_HOST=hwan515.synology.me
 export DB_PORT=15432
-export DB_NAME=recipe
+export DB_NAME=postgres
 export SECRET_KEY=b'X2t\x05\xf5\xcb\xc1\xa5`\xd6"\xfb \x0c\x14\xb5'
 export JWT_SECRET_KEY=b'\x10\xc8\xf9S\x1c&\x9fAD\x82\xf5\xa1\xdbx\xea>'
