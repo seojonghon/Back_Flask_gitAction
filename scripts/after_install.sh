a36f84d4725406d5e14a987127f624e57f441e1d
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-27 15:54:19 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -15,5 +15,16 @@ rm -rf appspec.yml requirements.txt
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/recipe_BE
 
-echo echo ">>> start server -----------"
-flask run --host=0.0.0.0
+echo ">>> set env --------------------------"
+chmod +x /home/ubuntu/recipe_BE/scripts/env.sh
+cd /home/ubuntu/recipe_BE/scripts
+source ./env.sh
+
+cd   /home/ubuntu/recipe_BE
+
+flask db init
+flask db migrate
+flask db upgrade
+
+echo ">>> start server ---------------------"
+nohup flask run --host=0.0.0.0 > log.txt 2>&1 &
