768f229a0d44096b11b5fb7c71966277418d3f1c
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-27 19:57:47 +0900

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,10 +2,10 @@ echo ">>> set env --------------------------"
 export FLASK_APP=app
 export REDIS_HOST=redis
 export APP_CONFIG_FILE=/home/ubuntu/recipe_BE/config/production.py
-export DB_USER=postgres
+export DB_USER=admin
 export DB_PASSWORD=password
-export DB_HOST=hwan515.synology.me
-export DB_PORT=15432
-export DB_NAME=postgres
+export DB_HOST=seo-mysql.crb6ck91tyxw.us-east-1.rds.amazonaws.com
+export DB_PORT=3306
+export DB_NAME=recipe
 export SECRET_KEY=b'X2t\x05\xf5\xcb\xc1\xa5`\xd6"\xfb \x0c\x14\xb5'
 export JWT_SECRET_KEY=b'\x10\xc8\xf9S\x1c&\x9fAD\x82\xf5\xa1\xdbx\xea>'
