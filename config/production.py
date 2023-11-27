6f485179164a6a36828218bd4792ae98260d933c
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-27 20:08:21 +0900

    Update production.py

--- config/production.py
+++ config/production.py
@@ -12,7 +12,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 # 시크릿 키 설정
 SECRET_KEY = os.getenv('SECRET_KEY')
