c1bff1f06dec5f0cda4bfe5e6f662466f1ef5d3c
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-27 14:31:18 +0900

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -9,3 +9,8 @@ export DB_PORT=15432
 export DB_NAME=postgres
 export SECRET_KEY=b'X2t\x05\xf5\xcb\xc1\xa5`\xd6"\xfb \x0c\x14\xb5'
 export JWT_SECRET_KEY=b'\x10\xc8\xf9S\x1c&\x9fAD\x82\xf5\xa1\xdbx\xea>'
+
+
+flask db init
+flask db migrate
+flask db upgrade
