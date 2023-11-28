0b2ce1367561795b76e66174752064d30fd48bb9
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-28 16:12:11 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -20,9 +20,16 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/recipe_BE/
 touch /home/ubuntu/recipe_BE/log.txt
 
 echo ">>> set env --------------------------"
-chmod +x /home/ubuntu/recipe_BE/scripts/env.sh
-cd /home/ubuntu/recipe_BE/scripts
-source ./env.sh
+export FLASK_APP=app
+export REDIS_HOST=redis
+export APP_CONFIG_FILE=/home/ubuntu/recipe_BE/config/production.py
+export DB_USER=admin
+export DB_PASSWORD=password
+export DB_HOST=hwan-db.cwjlce28o2o9.ap-southeast-1.rds.amazonaws.com
+export DB_PORT=3306
+export DB_NAME=recipe
+export SECRET_KEY=b'X2t\x05\xf5\xcb\xc1\xa5`\xd6"\xfb \x0c\x14\xb5'
+export JWT_SECRET_KEY=b'\x10\xc8\xf9S\x1c&\x9fAD\x82\xf5\xa1\xdbx\xea>'
 
 cd   /home/ubuntu/recipe_BE
 
