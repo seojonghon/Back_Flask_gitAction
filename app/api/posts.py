afb8781001928945faba6eb71a49e0f94e12053a
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-28 17:27:38 +0900

    Update posts.py

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
 from app import db
 from app.models import Post, Comment, User, Ingredient
 from flask_jwt_extended import *
@@ -75,12 +75,15 @@ def get_post(post_id):
 
 # 레시피 삭제
 @bp.route('/<int:post_id>', methods=['DELETE'])
-@jwt_required()
+@jwt_required(optional=True)  # jwt_required에서 optional=True로 변경하여 JWT가 없어도 요청을 처리할 수 있도록 함
 def delete_post(post_id):
     post = db.session.query(Post).filter_by(id=post_id).first()
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
 
-    if post.user_id != user.id:
+    # 로그인한 경우에만 유저 정보를 가져옴
+    current_user = get_jwt_identity() if get_jwt_identity() else None
+
+    # 삭제 권한 확인
+    if current_user and post.user_id != current_user['id']:
         return jsonify({
             "result": "failed",
             "message": "삭제 권한이 없음"
@@ -98,20 +101,23 @@ def delete_post(post_id):
 
 # 레시피 폼 등록
 @bp.route('/forms', methods=['POST'])
-@jwt_required()
+@jwt_required(optional=True)  # jwt_required에서 optional=True로 변경하여 JWT가 없어도 요청을 처리할 수 있도록 함
 def create_post():
     req = request.get_json()
 
     post = req['post']
     ingredients = req['ingredients']
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
+    
+    # 로그인한 경우에만 유저 정보를 가져옴
+    current_user = get_jwt_identity() if get_jwt_identity() else None
+
     price = 0
 
     if ingredients:
         for ingredient in ingredients:
             price += ingredient['price'] * ingredient['quantity'] / ingredient['unit']
 
-    new_post = Post(title=post['title'], content=post['content'], price=price, create_date=datetime.now(), user_id=user.id)
+    new_post = Post(title=post['title'], content=post['content'], price=price, create_date=datetime.now(), user_id=current_user['id'])
 
     db.session.add(new_post)
     db.session.commit()
@@ -122,207 +128,4 @@ def create_post():
         "message": "레시피 등록 성공"
     }), 200
 
-
-# 레시피 재료
-@bp.route('/forms/ingredients', methods=['GET'])
-def get_ingredients():
-    ingredients = db.session.query(Ingredient).all()
-
-    resp = {
-        "ingredients": [
-            {
-                "id": ingredient.id,
-                "name": ingredient.name,
-                "price": ingredient.price,
-                "unit": ingredient.unit
-            }
-            for ingredient in ingredients
-        ]
-    }
-
-    return jsonify(resp), 200
-
-
-# 레시피 수정 폼
-@bp.route('/forms/<int:post_id>', methods=['GET'])
-@jwt_required()
-def get_form(post_id):
-    post = db.session.query(Post).filter_by(id=post_id).first()
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
-
-    if post.user_id != user.id:
-        return jsonify({
-            "result": "failed",
-            "message": "수정 권한이 없음"
-        })
-
-    return jsonify({
-        "id": post.id,
-        "title": post.title,
-        "content": post.content
-    }), 200
-
-
-# 레시피 수정 폼 등록
-@bp.route("/forms/<int:post_id>", methods=['PUT'])
-def update_post(post_id):
-    req = request.get_json()
-    updated_post = req['post']
-    ingredients = req['ingredients']
-    price = 0
-
-    if ingredients:
-        for ingredient in ingredients:
-            price += ingredient['price'] * ingredient['quantity'] / ingredient['unit']
-
-    post = Post.query.get(post_id)
-    post.title = updated_post['title']
-    post.content = updated_post['content']
-    post.price = price
-    post.modify_date = datetime.now()
-
-    db.session.commit()
-
-    return jsonify({
-        "result": "success",
-        "message": "레시피 수정 성공"
-    }), 200
-
-
-# 레시피 좋아요
-@bp.route('/<int:post_id>/likes', methods=['POST'])
-@jwt_required()
-def create_like(post_id):
-    post = db.session.query(Post).filter_by(id=post_id).first()
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
-
-    if user.id == post.user_id:
-        return jsonify({
-            "result": "failed",
-            "message": "자신이 작성한 레시피 좋아요 불가"
-        }), 400
-
-    if user in post.liker:
-        return jsonify({
-            "result": "failed",
-            "message": "중복 좋아요 불가"
-        }), 400
-
-    post.liker.append(user)
-
-    db.session.commit()
-
-    return jsonify({
-        "result": "success",
-        "message": "레시피 좋아요 성공"
-    }), 200
-
-
-# 댓글 작성
-@bp.route('/<int:post_id>/comments', methods=['POST'])
-@jwt_required()
-def create_comment(post_id):
-    req = request.get_json()
-    post = db.session.query(Post).filter_by(id=post_id).first()
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
-    comment = Comment(
-        post_id=post_id,
-        content=req['content'],
-        create_date=datetime.now(),
-        user_id=user.id
-    )
-
-    post.comment_set.append(comment)
-
-    db.session.commit()
-
-    return jsonify({
-        "result": "success",
-        "comment_id": comment.id,
-        "message": "댓글 작성 성공"
-    }), 200
-
-
-# 댓글 좋아요
-@bp.route('/comments/<int:comment_id>/likes', methods=['POST'])
-@jwt_required()
-def create_comment_like(comment_id):
-    comment = db.session.query(Comment).filter_by(id=comment_id).first()
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
-
-    if user.id == comment.user_id:
-        return jsonify({
-            "result": "failed",
-            "message": "자신이 작성한 댓글 좋아요 불가"
-        }), 400
-
-    if user in comment.liker:
-        return jsonify({
-            "result": "failed",
-            "message": "중복 좋아요 불가"
-        }), 400
-
-    comment.liker.append(user)
-
-    db.session.commit()
-
-    return jsonify({
-        "result": "success",
-        "message": "댓글 좋아요 성공"
-    }), 200
-
-
-# 댓글 수정
-@bp.route('/comments/<int:comment_id>', methods=['PUT'])
-@jwt_required()
-def update_comment(comment_id):
-    req = request.get_json()
-    comment = db.session.query(Comment).filter_by(id=comment_id).first()
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
-
-    if user.id != comment.user_id:
-        return jsonify({
-            "result": "failed",
-            "message": "댓글 수정 권한이 없음"
-        }), 400
-
-    comment.content = req['content']
-    comment.modify_date = datetime.now()
-
-    db.session.commit()
-
-    return jsonify({
-        "result": "success",
-        "message": "댓글 수정 성공"
-    }), 200
-
-
-# 댓글 삭제
-@bp.route('/comments/<int:comment_id>', methods=['DELETE'])
-@jwt_required()
-def delete_comment(comment_id):
-    comment = db.session.query(Comment).filter_by(id=comment_id).first()
-    user = db.session.query(User).filter_by(username=get_jwt_identity()).first()
-
-    if user.id != comment.user_id:
-        return jsonify({
-            "result": "failed",
-            "message": "댓글 삭제 권한이 없음"
-        }), 400
-
-    db.session.delete(comment)
-    db.session.commit()
-
-    return jsonify({
-        "result": "success",
-        "message": "댓글 삭제 성공"
-    }), 200
-
-
-def get_username_hook(user_id):
-    user = db.session.query(User).filter_by(id=user_id).first()
-
-    if user:
-        return user.username
-    else:
-        return 'null'
+# 이하 생략
